41e0fe7b1f045fbfc8dff7d51cae0001def85b10
Dmitry Kalashnikov <dmkalash.mail@gmail.com>
2021-06-03 23:26:42 +0300

    errors fixed

--- src/network/st_nonblocking/ServerImpl.cpp
+++ src/network/st_nonblocking/ServerImpl.cpp
@@ -32,7 +32,10 @@ namespace STnonblock {
 ServerImpl::ServerImpl(std::shared_ptr<Afina::Storage> ps, std::shared_ptr<Logging::Service> pl) : Server(ps, pl) {}
 
 // See Server.h
-ServerImpl::~ServerImpl() {}
+ServerImpl::~ServerImpl() {
+    Stop();
+    Join();
+}
 
 // See Server.h
 void ServerImpl::Start(uint16_t port, uint32_t n_acceptors, uint32_t n_workers) {
@@ -93,9 +96,7 @@ void ServerImpl::Stop() {
     }
 
     for (auto connection : _connections) {
-        connection->OnClose();
-        close(connection->_socket);
-        delete(connection);
+        shutdown(connection->_socket, SHUT_RD);
     }
 
     close(_server_socket);
@@ -104,7 +105,9 @@ void ServerImpl::Stop() {
 // See Server.h
 void ServerImpl::Join() {
     // Wait for work to be complete
-    _work_thread.join();
+    if (_work_thread.joinable()) {
+        _work_thread.join();
+    }
 }
 
 // See ServerImpl.h
